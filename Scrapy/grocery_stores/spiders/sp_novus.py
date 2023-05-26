5cc05659e42e57d7de94b0be657acbda2d6ad90a
ArtemSerdechnyi <artem.serdechnyy@gmail.com>
2023-05-26 22:29:55 +0300

    create def add_page_to_url in sp_novus

--- Scrapy/grocery_stores/spiders/sp_novus.py
+++ Scrapy/grocery_stores/spiders/sp_novus.py
@@ -1,7 +1,7 @@
 import scrapy
 from scrapy_splash import SplashRequest
 
-from .common import DefaultSpider, DefaultLuaSpider
+from .common import DefaultSpider
 
 
 class NovusSpider(scrapy.Spider, DefaultSpider):
@@ -18,17 +18,38 @@ class NovusSpider(scrapy.Spider, DefaultSpider):
     #                        'https://novus.online/category/alkogol'
     #                        'https://novus.online/category/tutunovi-virobi-ta-aksesuari')
     start_urls_category = ('https://novus.online/category/ovochi-frukty-ta-horikhy',
-                           'https://novus.online/category/molocne-ajca-sir',
                            'https://novus.online/category/maso-riba')
 
-    def start_requests(self):
-        for url_category in self.start_urls_category:
-            yield from self.splash_request(urls=url_category,
-                                           callback=self.get_url_subcategory)
+    @staticmethod
+    def add_page_to_url(url: str):
+        page_number = 1
+        page_pattern = ''
+        for _ in range(999):
+            if page_number > 1:
+                page_pattern = f'/page-{page_number}'
+            yield f'{url}{page_pattern}'
+            page_number += 1
+        raise StopIteration(f'Page number is over 999 for {url}!')
 
-    def get_url_subcategory(self, response: scrapy.http.Response):
+    def start_requests(self) -> SplashRequest:
+        for url_category in self.start_urls_category:
+            yield from self.splash_request(url_s=url_category,
+                                           callback=self.parse_url_subcategory)
+
+    def parse_url_subcategory(self, response: scrapy.http.Response) -> SplashRequest:
         subcategory_list = response.xpath("//div[@class='subcategories-list__item']/a/@href").getall()
         for subcategory in subcategory_list:
             subcategory_url = response.urljoin(subcategory)
-            print('-----', subcategory_url)
+            yield from self.splash_request(url_s=subcategory_url,
+                                           callback=self.parse_subcategory)
 
+    def parse_subcategory(self, response: scrapy.http.Response):
+        partial_product_url_list = response.xpath(
+            "//a[@class='base-is-link base-card catalog-products__item']/@href").getall()
+        product_url_list = map(response.urljoin, partial_product_url_list)
+        for product_url in product_url_list:
+            yield from self.splash_request(url_s=product_url,
+                                           callback=self.parse_subcategory)
+
+    def parse_product(self, response: scrapy.http.Response):
+        product_parameters = {}
