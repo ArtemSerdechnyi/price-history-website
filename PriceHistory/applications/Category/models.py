30158742d6a71f7959691538f608a2f75f722c2e
ArtemSerdechnyi <artem.serdechnyy@gmail.com>
2023-07-06 18:02:32 +0300

    create model RawSubcategoryRelation

--- PriceHistory/applications/Category/models.py
+++ PriceHistory/applications/Category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ..Core.models import Shop
 
 
 class Category(models.Model):
@@ -12,11 +13,23 @@ class Category(models.Model):
 
 
 class Subcategory(models.Model):
-    subcategory_name = models.CharField(unique=True)
+    subcategory = models.CharField(unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.category} --> {self.subcategory_name}'
+        return f'{self.category} --> {self.subcategory}'
 
     class Meta:
         db_table = 'subcategory'
+
+
+class RawSubcategoryRelation(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    raw_subcategory_url = models.URLField(unique=True, primary_key=True)
+    subcategory = models.ManyToManyField(Subcategory)
+
+    def __str__(self):
+        return f'{self.subcategory} --> {self.raw_subcategory_url}'
+
+    class Meta:
+        db_table = 'raw_subcategory'
